6d898cf8e0d90b1a500c06942be44386643c767f
ewsgit <ewsgit@ewsgit.uk>
2025-11-30 23:36:29 +0000

    add backend services

---
+++ instance/meta/backend.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cd /var/www/workspaces/instance/ || {
+    echo "Failed to change directory to /var/www/workspaces/instance/"
+    exit 1
+}
+
+bun /var/www/workspaces/instance/src/index.ts
