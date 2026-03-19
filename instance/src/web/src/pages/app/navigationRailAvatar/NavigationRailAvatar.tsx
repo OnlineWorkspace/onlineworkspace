0ee3ac22520b94b8be229c70f3f1034570fc50cc
ewsgit <ewsgit@ewsgit.uk>
2026-03-19 19:27:20 +0000

    add logout button to the non-expanded application navigationRail

--- instance/src/web/src/pages/app/navigationRailAvatar/NavigationRailAvatar.tsx
+++ instance/src/web/src/pages/app/navigationRailAvatar/NavigationRailAvatar.tsx
@@ -9,47 +9,47 @@ import { useNavigate } from "@solidjs/router";
 import useIsMobile from "@tcsw/uikit-solid/src/core/useIsMobile.ts";
 
 const NavigationRailAvatar: Component<{ expanded: boolean }> = (props) => {
-    const navigate = useNavigate();
-    const isMobile = useIsMobile();
-    const [user] = createResource(() => trpc.app.navigation.user.name.query());
+  const navigate = useNavigate();
+  const isMobile = useIsMobile();
+  const [user] = createResource(() => trpc.app.navigation.user.name.query());
 
-    return (
-        <div class={styles.root} data-expanded={props.expanded}>
-            <UKAvatar
-                onClick={() => {
-                    navigate("/app/uk.tcsw.settings/profile");
-                }}
-                class={styles.avatar}
-                avatar={backend("/api/user/me/avatar/s")}
-                size="s"
-                username="[PUT SIDEBAR HERE]"
-            />
-            <Show when={!isMobile()}>
-                <div class={styles.nameContainer}>
-                    <UKText size="m" role="title" class={styles.displayName}>
-                        {`${user()?.forename} ${user()?.surname}`}
-                    </UKText>
-                    <UKText size="m" role="label" class={styles.username}>
-                        {`@${user()?.username}`}
-                    </UKText>
-                </div>
-                <UKIconButton
-                    class={styles.logout}
-                    icon={"logout"}
-                    alt={"Logout"}
-                    color={"filled"}
-                    shape={"round"}
-                    size={"s"}
-                    width={"default"}
-                    onClick={async () => {
-                        await trpc.authorization.logout.mutate();
-
-                        navigate("/");
-                    }}
-                ></UKIconButton>
-            </Show>
+  return (
+    <div class={styles.root} data-expanded={props.expanded}>
+      <UKAvatar
+        onClick={() => {
+          navigate("/app/uk.tcsw.settings/profile");
+        }}
+        class={styles.avatar}
+        avatar={backend("/api/user/me/avatar/s")}
+        size="s"
+        username="me"
+      />
+      <Show when={!isMobile()}>
+        <div class={styles.nameContainer}>
+          <UKText size="m" role="title" class={styles.displayName}>
+            {`${user()?.forename} ${user()?.surname}`}
+          </UKText>
+          <UKText size="m" role="label">
+            {`@${user()?.username}`}
+          </UKText>
         </div>
-    );
+        <UKIconButton
+          class={styles.logout}
+          icon={"logout"}
+          alt={"Logout"}
+          color={"filled"}
+          shape={"round"}
+          size={"s"}
+          width={"default"}
+          onClick={async () => {
+            await trpc.authorization.logout.mutate();
+
+            navigate("/");
+          }}
+        ></UKIconButton>
+      </Show>
+    </div>
+  );
 };
 
 export default NavigationRailAvatar;
