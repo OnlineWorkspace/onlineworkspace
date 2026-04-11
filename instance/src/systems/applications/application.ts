ac0ca5a612168814ea11e9c5601b4a5fdfdd110f
ewsgit <ewsgit@ewsgit.uk>
2026-04-11 16:33:02 +0000

    add application banners

--- instance/src/systems/applications/application.ts
+++ instance/src/systems/applications/application.ts
@@ -1,25 +1,31 @@
 export interface WorkspacesApplication {
-    id: string;
-    // if undefined, just the id is used
-    displayName?: string;
-    icon?: {
-        type: "icon" | "image";
-        value: string;
-    };
-    description?: string;
-    authors: string[];
-    license: string;
-    source?: string;
-    graphicsAcceleration?: string;
-    modules: {
-        // not yet supported
-        docker?: {}[];
-        // located at /app/[id]/
-        web?: { path: string };
-        // run bun ./[path.ts]
-        bun?: { path: string };
-        // run ./[path]
-        script?: { path: string };
-    };
-    version?: string;
+  id: string;
+  // if undefined, just the id is used
+  displayName?: string;
+  icon?: {
+    type: "icon" | "image";
+    value: string;
+  };
+  description?: string;
+  authors: string[];
+  license: string;
+  source?: string;
+  dependsOn?: {
+    applications: {
+      id: string;
+      repository?: string;
+    }[];
+  };
+  graphicsAcceleration?: string;
+  modules: {
+    // NOTICE: not yet supported
+    docker?: object[];
+    // located at /app/[id]/
+    web?: { path: string };
+    // run bun ./[path.ts]
+    bun?: { path: string };
+    // run ./[path]
+    script?: { path: string };
+  };
+  version?: string;
 }
